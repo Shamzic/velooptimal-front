bbff8a158d88f23781dcb37ba06305c68ffda53b
Shamzic <shamery@free.fr>
2023-11-27 20:23:31 +0100

    chore: configure les pages nuxt avec le header

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+        <ul>
+            <li><NuxtLink to="/">Optimal Gear</NuxtLink></li>
+            <li><NuxtLink to="/camper">Camping</NuxtLink></li>
+            <li><NuxtLink to="/trail">Trail</NuxtLink></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const title = ref('Optimal Gear');
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <NuxtPage />
+    Bienvenue sur Optimal Gear, le site spécialisé dans le matériel outdoor qui conviendra à VORTE pratique et à VOTRE budget !
+  </div>
+</template>
