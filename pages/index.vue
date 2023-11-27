12058f1ca23c378985727850e2efa7462b5df302
Shamzic <shamery@free.fr>
2023-11-27 20:41:33 +0100

    chore: center the home page title

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtPage />
-    Bienvenue sur Optimal Gear, le site spécialisé dans le matériel outdoor qui conviendra à VORTE pratique et à VOTRE budget !
+    <h3 class="text-center text-lg font-bold m-8">Bienvenue sur Optimal Gear, le site spécialisé dans le matériel outdoor qui conviendra à VORTE pratique et à VOTRE budget !</h3>
   </div>
 </template>
