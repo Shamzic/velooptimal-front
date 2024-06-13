f25d82400f02fb96643067a3f76eb4979c3d6330
Shamzic <shamery@free.fr>
2024-06-13 18:02:17 +0200

    test post server

---
+++ pages/server.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const body = ref('')
+
+async function submit() {
+  body.value = await $fetch('/api/submit', {
+    method: 'post',
+    body: { test: 123 }
+  })
+}
+
+</script>
+
+<template>
+    <div>
+        <button @click="submit">Submit</button>
+    </div>
+  <div>
+    {{ body }}
+  </div>
+</template>
