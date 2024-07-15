050ca0f8b2fd966a1bf1f9977ca5f05cd4181256
Shamzic <shamery@free.fr>
2024-07-15 16:49:15 +0200

    feat: add footer & improve (header + app layout)

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
-  <Header></Header>
-  <NuxtPage />
+  <div class="flex flex-col h-screen justify-between">
+    <Header class="h-32" />
+    <NuxtPage class="grow" />
+    <Footer  class="h-56" />
+  </div>
 </template>
--- components/header.vue
+++ components/header.vue
@@ -1,22 +1,24 @@
 
 <template>
-    <div class="navbar bg-base-100">
+    <div class="navbar bg-base-200">
   <div class="navbar-start">
     <div class="dropdown">
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><NuxtLink to="/camper">Camping</NuxtLink></li>
-        <li><NuxtLink to="/trail">Trail</NuxtLink></li>
+      <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52" id="menu">
+        <li><NuxtLink to="/gravel">Gravel</NuxtLink></li>
+        <li><NuxtLink to="/mtb">VTT</NuxtLink></li>
+        <li><NuxtLink to="/road">Route</NuxtLink></li>
       </ul>
     </div>
-    <NuxtLink to="/" class="btn btn-ghost text-xl">Optimal Gear</NuxtLink>
+    <NuxtLink to="/" class="btn btn-success glass text-xl">Optimal Bikes</NuxtLink>
   </div>
   <div class="navbar-center hidden lg:flex">
     <ul class="menu menu-horizontal px-1 space-x-2">
-        <li class="btn btn-accent btn-outline"><NuxtLink to="/camper">Camping</NuxtLink></li>
-        <li class="btn btn-accent btn-outline"><NuxtLink to="/trail">Trail</NuxtLink></li>
+        <NuxtLink class="btn btn-success glass" to="/gravel">Gravel</NuxtLink>
+        <NuxtLink class="btn btn-success glass" to="/mtb">VTT</NuxtLink>
+        <NuxtLink class="btn btn-success glass" to="/road">Route</NuxtLink>
     </ul>
   </div>
 </div>
@@ -26,4 +28,5 @@
 import { ref } from 'vue';
 
 const title = ref('Optimal Gear');
+
 </script>
