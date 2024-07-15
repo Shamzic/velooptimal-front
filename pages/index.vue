c33dbd86a80522edb5585aa6f2b1fd4ee10759d2
Shamzic <shamery@free.fr>
2024-07-15 16:49:24 +0200

    refactor: pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtPage />
-    <h3 class="text-center text-lg font-bold m-8">Bienvenue sur Optimal Gear, le site spécialisé dans le matériel outdoor qui conviendra à VORTE pratique et à VOTRE budget !</h3>
+    <h3 class="text-center text-lg font-bold m-8">Vous cherchez un vélo optimal, adapté à votre pratique et votre budget ?</h3>
   </div>
 </template>
