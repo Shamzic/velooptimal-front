610059af9788a9f83dd3b717f8e7d3d6cdb9d92a
Shamzic <shamery@free.fr>
2024-07-26 17:37:55 +0200

    feat: add meta tag impact-site-verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      meta: [
+        {
+          name:'impact-site-verification',
+          content: '3e1dce80-7864-4a07-82a2-593e299ab0e4'
+        },
+        {
+          name:'impact-site-verification',
+          content: 'f4dfb430-55ce-4d02-8bfc-1eb4d0948cd7'
+        },
+      ]
+    }
+  }
 })
